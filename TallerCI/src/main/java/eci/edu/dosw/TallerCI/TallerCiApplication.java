967dfc972497f7f4737d41cea92c9bec2514b2fc
Sebastian Barros <sebastian.barros-b@mail.escuelaing.edu.co>
2025-10-23 15:51:28 -0500

    estructura

---
+++ TallerCI/src/main/java/eci/edu/dosw/TallerCI/TallerCiApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.TallerCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerCiApplication.class, args);
+	}
+
+}
---
+++ TallerCI/src/test/java/eci/edu/dosw/TallerCI/TallerCiApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.TallerCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
